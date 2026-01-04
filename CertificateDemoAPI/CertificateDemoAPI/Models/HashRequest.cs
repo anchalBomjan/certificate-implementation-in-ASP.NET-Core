19637e103f8859805bfaec7d21ed82636bdf2274
anchal.lama <anchal.lama@example.com>
2026-01-04 22:27:26 +0545

    Adding Certificated

--- CertificateDemoAPI/CertificateDemoAPI/Models/HashRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CertificateDemoAPI.Models
-{
-    public class HashRequest
-    {
-        public string HashBase64 { get; set; }
-        public string Algorithm { get; set; } = "SHA256";
-    }
-}
